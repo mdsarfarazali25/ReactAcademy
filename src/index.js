23df829b20d25a5f2bbfb5df47ac138c9f84522d
Mohammed Sarfaraz Ali <mdsarfarazali025@gmail.com>
2025-06-11 14:44:50 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+ <BrowserRouter basename="/ReactAcademy">
     <App />
   </BrowserRouter>
 );
